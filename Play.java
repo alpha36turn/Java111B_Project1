d6f0012648887a64103bc039d16f267bd9e36e04
Segev Malool <segevmalool@yahoo.com>
2016-08-23 12:57:23 -0700

    first

---
+++ Play.java
@@ -0,0 +1,10 @@
+package Project1;
+import java.util.*;
+
+public class Play
+{
+    public static void main(String[] args)
+    {
+        Raffle r = new Raffle();
+    }
+}
---
+++ Raffle.java
@@ -0,0 +1,27 @@
+package Project1;
+import java.util.*;
+
+public class Raffle
+{
+    private int min;
+    private int max;
+
+    public Raffle()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lowest ticket number: ");
+        min = -1;
+        while(true) {
+            min = sc.nextInt();
+            if (min <= 0) System.out.println("Lowest ticket number must be positive!");
+            else break;
+        }
+
+        System.out.println("Highest ticket number: ");
+        while(true) {
+            max = sc.nextInt();
+            if (max-min < 3) System.out.println("Must have at least 3 tickets!");
+            else break;
+        }
+    }
+}
